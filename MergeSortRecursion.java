f484484f59f674c952e81ede29cecaa7093d06ff
Aansh Ojha <1aanshojha@gmail.com>
2023-10-31 16:38:42 +0530

    Create MergeSortRecursion.java

---
+++ MergeSortRecursion.java
@@ -0,0 +1,116 @@
+package DSA.Recursion.Array.Sorting;
+
+import java.util.Arrays;
+
+public class MergeSortRecursion {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        mergeSortInPlace(arr, 0, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void mergeSortInPlace(int[] arr, int start, int end) {
+        if (end - start == 1) {     // i.e. when one element left
+            return;
+        }
+
+        int mid = (start + end) / 2;
+
+        // Keep splitting array into half till one element left
+        mergeSortInPlace(arr, start, mid);      // For first half of array
+        mergeSortInPlace(arr, mid, end);        // After first half, time for second half
+
+        // Base condition met then
+        mergeInPlace(arr, start, mid, end);
+    }
+
+    private static void mergeInPlace(int[] arr, int start, int mid, int end) {
+
+        // Add sorted element to new array mix[] then copy mix[] to original arr[]
+
+        int[] mix = new int[end - start];
+
+        int i = start;  // i goes from start to mid - 1
+        int j = mid;    // j goes from mid to end - 1
+        int k = 0;      // pointer for new array mix[]
+
+        while (i < mid && j < end) {
+            if (arr[i] < arr[j]) {
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // it may be possible that one of the arrays is completed other still has element in it
+        // Means after merging, some elements left so,
+        // copy the remaining elements
+        while (i < mid) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while (j < end) {
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        System.arraycopy(mix, 0, arr, start, mix.length);
+    }
+
+
+    // Inefficient method AHEAD, as creates new array at every step  ------>>>
+
+    static int[] mergeSort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+
+        int mid = arr.length / 2;
+
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
+    }
+
+    private static int[] merge(int[] first, int[] second) {
+        int[] mix = new int[first.length + second.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < first.length && j < second.length) {
+            if (first[i] < second[j]) {
+                mix[k] = first[i];
+                i++;
+            } else {
+                mix[k] = second[j];
+                j++;
+            }
+            k++;
+        }
+
+        // it may be possible that one of the arrays is not complete
+        // copy the remaining elements
+        while (i < first.length) {
+            mix[k] = first[i];
+            i++;
+            k++;
+        }
+
+        while (j < second.length) {
+            mix[k] = second[j];
+            j++;
+            k++;
+        }
+
+        return mix;
+    }
+}
